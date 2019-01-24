d77bccc7f5b8af2be963d0ec006cab2fa9ab3397
Diallo Thierno <diallosorycosa@gmail.com>
2019-01-24 09:28:50 +0100

    Structure finale

---
+++ PhotoNum/src/models/Utilisateur.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Utilisateur {
+	
+	private String mail;
+
+	public Utilisateur(String mail) {
+		super();
+		this.mail = mail;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+}
