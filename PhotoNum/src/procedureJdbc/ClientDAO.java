a2b5f230170dfdd917f87a1ab6fa8cbbfe8cd188
ali jouhari <ali.j.199316@gmail.com>
2019-01-24 17:57:13 +0100

    jdbc client

--- PhotoNum/src/procedureJdbc/ClientDAO.java
+++ PhotoNum/src/procedureJdbc/ClientDAO.java
@@ -1,12 +1,27 @@
 package procedureJdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import models.Client;
 
 public class ClientDAO extends DAO<Client>{
-
+    Connection conn;
+	
 	@Override
 	public Client find(String id) {
-		// TODO Auto-generated method stub
+	   	Statement stmt;
+		try {
+			stmt = conn.createStatement();
+	    	String query1 = "Select * from Utilisateur where mailU = '"+id+"' ";
+	    	ResultSet rs = stmt.executeQuery(query1);
+	    	if(rs.next())
+	    	  return new Client(rs.getString("mailC"), rs.getString("nomC") , rs.getString("prenomC") , rs.getString("mdpasseC"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -18,20 +33,50 @@ public class ClientDAO extends DAO<Client>{
 
 	@Override
 	public Client create(Client obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	   try {
+		   Statement stmt = conn.createStatement();
+		   String query1 = "Select mailU from Utilisateur where mailU = '"+obj.getMail()+"' ";
+		   ResultSet rs = stmt.executeQuery(query1);
+		   if(!rs.next()) {
+	 		 String query2 = "INSERT INTO Utilisateur Values ('"+obj.getMail()+"')";
+	 		 String query3 = "INSERT INTO ClientP Values ('"+obj.getMail()+"','"+obj.getNom()+"','"+obj.getPrenom()+"','"+obj.getPassword()+"')";
+	 		 stmt.executeQuery(query2);
+	 		 stmt.executeQuery(query3);
+	 		 return obj;
+		   } else {
+	 		   System.out.println("mail déja utilisé");
+		   }
+	   	} catch (SQLException e) {
+		   e.printStackTrace();
+	   		}
+	   return null; 
+    }
 
 	@Override
 	public Client update(Client obj) {
-		// TODO Auto-generated method stub
+		try {
+			Statement stmt = conn.createStatement();
+			String query = "Update ClientP SET nomC ='"+obj.getNom()+"' and '"+obj.getPrenom()+"' and '"+obj.getPassword()+"'";
+			stmt.executeQuery(query);
+			
+			return obj;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
 	public void delete(Client obj) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Statement stmt = conn.createStatement();
+			String query = "DELETE ON ClientP where mailC = '"+obj.getMail()+"'";
+			stmt.executeQuery(query);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
